49360b93f780eacae53d7a7ff1cb581fec0f695d
KwonAYeong <129161150+KwonAYeong@users.noreply.github.com>
2025-04-03 16:40:06 +0900

    Init

---
+++ src/main/java/com/kkks/pofolling/PofollingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kkks.pofolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PofollingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PofollingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkks/pofolling/PofollingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkks.pofolling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PofollingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

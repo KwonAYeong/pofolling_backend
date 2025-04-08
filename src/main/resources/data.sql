0df63e24177da661ddf58b880dbfc297730e740f
KwonAYeong <129161150+KwonAYeong@users.noreply.github.com>
2025-04-08 11:10:56 +0900

    chore: 더미 유저 데이터 및 데이터 초기화 설정 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (user_id, email, password, name, nickname, role, is_verified)
+VALUES (1, 'kay@example.com', '1234', 'KAY', 'kay', 'MENTEE', false);
+
+INSERT INTO users (user_id, email, password, name, nickname, role, is_verified)
+VALUES (2, 'khg@example.com', '1234', 'KHG', 'khg', 'MENTEE', false);
+
+INSERT INTO users (user_id, email, password, name, nickname, role, is_verified)
+VALUES (3, 'khm@example.com', '1234', 'KHM', 'khm', 'MENTOR', false);
+
+INSERT INTO users (user_id, email, password, name, nickname, role, is_verified)
+VALUES (4, 'sjy@example.com', '1234', 'SJY', 'sjy', 'MENTOR', false);
